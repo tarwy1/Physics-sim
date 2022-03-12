fe12ef5aa65d4a675350e3b7832b04265590f2f7
tarwy <headoncharlie@gmail.com>
2022-03-12 16:44:08 +0000

    IT DRAWS!

--- physics sim/Main.cpp
+++ physics sim/Main.cpp
@@ -13,15 +13,34 @@ namespace Phi {
         constexpr std::size_t newtonSqrtIterations = 4;
         namespace Constant {
             constexpr double grav = 9.80665; // m/s^2
-            constexpr double pi = 3.141593; // unitless
+            constexpr double pi = 3.14159265358979323846; // unitless
             constexpr double stdTemp = 20; // deg C
             constexpr double stdKelvin = 293; // K
             constexpr double stdPressure = 101325; // Pa
             constexpr double Kelvin = 273.15; // K @ 0degC
             constexpr double stdDens = 1.2041; // Kg/m^3
             constexpr double lightspeed = 299792458; // m/s
+            constexpr int pixelsPerMeter = 10;
         }
 
+        double factorial(int num)
+        {
+            double factans = 1;
+            while (num != 1 && num != 0) {
+                factans *= num;
+                num--;
+            }
+            return factans;
+        }
+        double quickExponent(double a, int b) // only works for natural number exponents
+        {
+            if (b == 0) return 1;
+            double ans = 1;
+            for (int i = 0; i < b; i++) {
+                ans *= a;
+            }
+            return ans;
+        }
         double sqrt(const double x) {
             if(!x) return 0;
 
@@ -147,6 +166,7 @@ namespace Phi {
         } type;
 
         std::vector<Vector2D> points;
+        std::vector<Vector2D> translatedPoints;
 
         bool closed = true;
 
@@ -156,14 +176,18 @@ namespace Phi {
             double radius; // m
         };
 
+
         Shape(const double radius) : type(ShapeType::Circle), radius(radius) {
             findAreaPerim();
+            for (double i = 0; i < 2 * Math::Constant::pi; i += 0.01) {
+                points.push_back({ radius * cos(i) , radius * sin(i) });
+            }
         }
         Shape(const ShapeType type, const double length, const double width = 0.0) : type(type), length(length), width(width == 0.0 ? length : width) {
-            findAreaPerim();
+            //findAreaPerim();
         }
         Shape(const ShapeType type) : type(type) {
-            findAreaPerim();
+            //findAreaPerim();
         }
 
 
@@ -171,7 +195,7 @@ namespace Phi {
         double area; // m^2
         double perimeter; // m
 
-        void findAreaPerim() {
+        void findAreaPerim() { // SOMETHING HERE CAUSES AN ERROR "cannot seek value intialised vector iterator" line 110
             perimeter = 0;
             area = 0;
             switch(this->type) {
@@ -228,22 +252,44 @@ namespace Phi {
 
         double mass; // kg
         double staticFriction = 0.0; // 0 < u < 1
-        double kineticFriction = 0; // 0 < u < 1
+        double kineticFriction = 0.0; // 0 < u < 1
 
         bool fixed = true; // Whether the body can move
         double angularDamp = 0.1; // Constant friction against angular motion  rad/s
 
         bool rigid = true; // Cannot deform
-
-        Body(const Shape& shape, double mass, double staticFriction = 0.0, double kineticFriction = 0.0, double rotation = 0.0, double radius = 0.0, double length = 0.0, double width = 0.0) : mass(mass), staticFriction(staticFriction), kineticFriction(kineticFriction), rotation(rotation), shape(shape.type == Shape::ShapeType::Circle ? radius : shape.type == Shape::ShapeType::Square ? length : shape.type == Shape::ShapeType::Rectangle ? length, width : shape), position(){
+        void translate(Vector2D translation) {
+            this->shape.translatedPoints.clear();
+            for (int i = 0; i < this->shape.points.size(); i++) {
+                this->shape.translatedPoints.push_back({ this->shape.points[i].components[0] + translation.components[0], this->shape.points[i].components[1] + translation.components[1] });
+            }
+        }
+        struct defaultParams {
+            double staticFriction = 0.0;
+            double kineticFriction = 0.0;
+            double rotation = 0.0;
+            double radius = 0.0;
+            double length = 0.0;
+            double width = 0.0;
+        }; // this is here to allow for passing parameters by name, out of order
+        Body(double mass, defaultParams defaults, const Shape& shape = Shape::ShapeType::Polygon) : mass(mass), staticFriction(staticFriction), kineticFriction(kineticFriction), rotation(rotation), shape(shape.type == Shape::ShapeType::Circle ? defaults.radius : shape.type == Shape::ShapeType::Square ? defaults.length : shape.type == Shape::ShapeType::Rectangle ? defaults.length, defaults.width : shape) {
 
         }
     };
+    std::vector<Body> bodies;
 };
 
 
 void refresh() {
-
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_POINTS);
+    for (auto i = Phi::bodies.begin(); i != Phi::bodies.end(); ++i) {
+        for (auto j = i->shape.translatedPoints.begin(); j != i->shape.translatedPoints.end(); ++j) {
+            glVertex2i(GLint(j->components[0] * Phi::Math::Constant::pixelsPerMeter), GLint(j->components[1] * Phi::Math::Constant::pixelsPerMeter));
+        }
+    }
+    glEnd();
+    glFlush();
 }
 void reshape(int width, int height) {
 
@@ -252,16 +298,24 @@ void reshape(int width, int height) {
 int main(int argc, char** argv) {
     std::cout.precision(20);
 
-    Phi::Body kekw = Phi::Body(Phi::Shape(3.0), 1.0);
+    Phi::Body::defaultParams def;
+    def.radius = 5.0;
+    Phi::Body kekw = Phi::Body(2.0, def, Phi::Shape::ShapeType::Circle);
+    kekw.position.components[0] = 10;
+    kekw.position.components[1] = 10;
+    kekw.translate(kekw.position);
+    Phi::bodies.push_back(kekw);
 
     glutInit(&argc, argv);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(500, 400);
-    glutCreateWindow("Calculator");
+    glutCreateWindow("Physics sim");
     glClearColor(255, 255, 255, 255);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0.0, 500, 0.0, 500);
+    glPointSize(1.0);
+    glColor3ub(255, 0, 0);
     glutDisplayFunc(refresh);
     glutReshapeFunc(reshape);
     glutMainLoop();
